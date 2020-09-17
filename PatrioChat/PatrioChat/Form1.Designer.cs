be1002ebd0a004ccb0371293ba4c066c557fb0fc
guy bernstein <guy.berenshten@gmail.com>
2020-09-17 11:06:40 +0300

    created basic design of ui

---
+++ PatrioChat/PatrioChat/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace PatrioChat
+{
+    partial class PatrioChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatrioChat));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.Chats = new System.Windows.Forms.ListBox();
+            this.messagesBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe Print", 10F);
+            this.textBox1.Location = new System.Drawing.Point(138, 395);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(569, 52);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.chatsBox_TextChanged);
+            // 
+            // SendButton
+            // 
+            this.SendButton.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.SendButton.Font = new System.Drawing.Font("Segoe Script", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendButton.Location = new System.Drawing.Point(713, 395);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 52);
+            this.SendButton.TabIndex = 1;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = false;
+            this.SendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // Chats
+            // 
+            this.Chats.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Chats.FormattingEnabled = true;
+            this.Chats.ItemHeight = 28;
+            this.Chats.Location = new System.Drawing.Point(12, 23);
+            this.Chats.Name = "Chats";
+            this.Chats.Size = new System.Drawing.Size(120, 424);
+            this.Chats.TabIndex = 2;
+            this.Chats.SelectedIndexChanged += new System.EventHandler(this.chatsBox_SelectedIndexChanged);
+            // 
+            // messagesBox
+            // 
+            this.messagesBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.messagesBox.Font = new System.Drawing.Font("Segoe Print", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messagesBox.FormattingEnabled = true;
+            this.messagesBox.ItemHeight = 23;
+            this.messagesBox.Location = new System.Drawing.Point(138, 23);
+            this.messagesBox.Name = "messagesBox";
+            this.messagesBox.Size = new System.Drawing.Size(650, 349);
+            this.messagesBox.TabIndex = 3;
+            // 
+            // PatrioChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.messagesBox);
+            this.Controls.Add(this.Chats);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.textBox1);
+            this.Name = "PatrioChat";
+            this.Text = "PatrioChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.ListBox Chats;
+        private System.Windows.Forms.ListBox messagesBox;
+    }
+}
+
---
+++ PatrioChat/PatrioChat/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatrioChat
+{
+    public partial class PatrioChat : Form
+    {
+        public PatrioChat()
+        {
+            InitializeComponent();
+        }
+
+        private void chatsBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void chatsBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
